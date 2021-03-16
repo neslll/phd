d5912dd3ec3167406898df62548ab4607c42bcfa
neslll <neslihanozhann@gmail.com>
2021-03-16 15:09:17 +0000

    Drive integration

--- test.R
+++ test.R
@@ -1,1 +1,5 @@
-#test
+remotes::install_github("januz/rmdrive", force = TRUE)
+
+
+rmdrive::upload_rmd(file = "test_markdown")# specifies the local `.Rmd` file (without extension)
+rmdrive::update_rmd(file = "test_markdown")# specifies the local `.Rmd` file (without extension)
